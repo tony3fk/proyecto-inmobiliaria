72e616e18c49f8f21d842f14b9dd67b50120c4c4
tony3fk <tony3fk@yahoo.es>
2020-04-02 18:56:47 +0200

    galerias mismo tamaño ok 2

--- app/templates/mostrarInmuebles.php
+++ app/templates/mostrarInmuebles.php
@@ -3,39 +3,44 @@
 
 <!-- Grid column -->
 <div class="row">
+
+
     <?php foreach ($params['inmuebles'] as $inmuebles) : ?>
 
 
-    <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12">
+    <div class="col-xl-3 col-lg-4 col-md-6 col-xs-12" id="card">
         <!-- Card Dark -->
-        <div class="card" id="card">
+        <div class="card">
 
             <!-- Card image -->
-            <div class="view overlay">
-                <img class="card-img-top img-responsive p-2"
+            <div class="embed-responsive embed-responsive-4by3">
+
+                <img class="card-img-top embed-responsive-item"
                     src="../app/images/<?php echo $inmuebles['referencia'] . ".jpeg"; ?>" alt="Card image cap"
                     id="img-inmueble">
 
             </div>
 
             <!-- Card content -->
-            <div class="card-body elegant-color white-text rounded-bottom ">
+            <div class="card-body ">
 
 
                 <!-- Title -->
                 <h4 class="card-title">Precio:
                     <?php echo number_format($inmuebles['precio_venta'], 2, ',', '.') . " €" ?>
                 </h4>
+
+
                 <hr class="hr-light">
                 <!-- Text -->
 
-                <h6 class="card-text white-text mb-4 ">Provincia: <?php echo $inmuebles['provincia'] ?></h6>
+                <h6 class="card-text   ">Provincia: <?php echo $inmuebles['provincia'] ?></h6>
 
-                <p class="card-text white-text mb-4"><?php echo $inmuebles['tipo'] . " en " . $inmuebles['operacion'] ?>
+                <p class="card-text  "><?php echo $inmuebles['tipo'] . " en " . $inmuebles['operacion'] ?>
                 </p>
                 <!-- Link -->
                 <a href="index.php?ctl=verInmueble&referencia=<?php echo $inmuebles['referencia'] ?>"
-                    class="white-text d-flex justify-content-end">
+                    class="btn btn-primary">
                     <h5>Más info. </h5>
                 </a>
 
@@ -55,6 +60,7 @@
 
 </div>
 <!-- Grid column -->
+
 
 
 
