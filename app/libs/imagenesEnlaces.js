fc7e4466544d0dabc4c185ac546c11e80bde6f56
tony3fk <tony3fk@yahoo.es>
2020-05-17 17:24:01 +0200

    miniaturas en tablas

--- app/libs/imagenesEnlaces.js
+++ app/libs/imagenesEnlaces.js
@@ -1,7 +1,7 @@
 this.thumbPreview = function() {
         xOffset = 10, yOffset = 30, $("a.thumb").hover(function(t) {
             this.t = this.title, this.title = "";
-            var e = "" != this.t ? "<br>" + this.t : "";
+            var e = "" !== this.t ? "<br>" + this.t : "";
             $("body").append("<p id='thumb'><img width='300x' src='" + this.href + "' alt='' />" + e + "</p>"), $("#thumb").css("top", t.pageY - xOffset + "px").css("left", t.pageX + yOffset + "px").fadeIn("fast")
         }, function() {
             this.title = this.t, $("#thumb").remove()
